769f815817f225e4726642e1edfae2165b3a484f
Lasha Sherozia <repeop0@gmail.com>
2021-01-27 17:07:22 +0400

    Created models and inserted one data to each table. Registered models on admin page.

--- washing_service/admin.py
+++ washing_service/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(WashingCenter)
+admin.site.register(Manager)
+admin.site.register(Employees)
+admin.site.register(History)
+admin.site.register(ScheduledOrders)
--- washing_service/models.py
+++ washing_service/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+
+class WashingCenter(models.Model):
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.location
+
+    class Meta:
+        verbose_name = 'Washing Center'
+        verbose_name_plural = 'Washing Centers'
+
+
+class Manager(models.Model):
+    full_name = models.CharField(max_length=50)
+    location = models.OneToOneField(WashingCenter, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        verbose_name = 'Manager'
+        verbose_name_plural = 'Managers'
+
+
+class Employees(models.Model):
+    full_name = models.CharField(max_length=50)
+    location = models.ForeignKey(WashingCenter, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        verbose_name = 'Employee'
+        verbose_name_plural = 'Employees'
+
+
+class History(models.Model):
+    customer = models.CharField(max_length=50)
+    location = models.OneToOneField(WashingCenter, on_delete=models.CASCADE)
+    car = models.CharField(max_length=50)
+    date = models.DateTimeField()
+    phone = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.customer} - {self.car}"
+
+    class Meta:
+        verbose_name = 'History'
+        verbose_name_plural = 'Histories'
+
+
+class ScheduledOrders(models.Model):
+    customer = models.CharField(max_length=40)
+    location = models.OneToOneField(WashingCenter, on_delete=models.CASCADE)
+    car = models.CharField(max_length=50)
+    date = models.DateTimeField()
+    phone = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.customer} - {self.car} at {self.date}"
+
+    class Meta:
+        verbose_name = 'Scheduled Order'
+        verbose_name_plural = 'Scheduled Orders'
