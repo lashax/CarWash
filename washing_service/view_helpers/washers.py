64b514bc4f825fc875baa9231a8e401db9e6b0fa
Lasha Sherozia <repeop0@gmail.com>
2021-02-07 08:09:13 +0400

    Updated view_helpers/washers file. Code is more readable and efficient.

--- washing_service/view_helpers/washers.py
+++ washing_service/view_helpers/washers.py
@@ -5,6 +5,29 @@ from django.db.models import Count, Q, F, DecimalField
 from washing_service.models import CarType, Washer
 
 
+def gen_query(car_type, history_date, time_interval):
+    """
+    Generate query, Q object which will be passed to Count class as
+    filter argument.
+
+    car_type is type of a car with which to filter history. This should be
+    one of: 'Sedan', 'Minivan, 'Truck'.
+
+    If time_interval parameter is None, that means query is used
+    to find out overall salary of an user, in which case history is not
+    filtered with date.
+
+    return Q object.
+    """
+    if time_interval:
+        return Q(**{
+            'history__car_type': CarType.objects.get(type=car_type),
+            history_date: time_interval
+        })
+    else:
+        return Q(**{'history__car_type': CarType.objects.get(type=car_type)})
+
+
 def earning_by_time(filter_arg):
     """
     Given 'filter_arg' variable, which is either
@@ -17,42 +40,30 @@ def earning_by_time(filter_arg):
     as to how much has the washer earned in the given interval, also considering
     salary %.
     """
+    time_dict = {
+        'overall': None,
+        'year': datetime.now().year,
+        'month': datetime.now().month,
+        'week': datetime.now().isocalendar()[1]
+    }
 
-    if filter_arg == 'year':
-        current = datetime.now().year
-    elif filter_arg == 'month':
-        current = datetime.now().month
-    elif filter_arg == 'week':
-        current = datetime.now().isocalendar()[1]
-    elif filter_arg == 'overall':
-        earning = Washer.objects.annotate(
-            overall=(10 * Count('history', filter=
-            Q(history__car_type=CarType.objects.get(type='Sedan')),
-                                output_field=DecimalField())
-                     + 15 * Count('history', filter=Q(
-                        history__car_type=CarType.objects.get(type='Minivan')),
-                                  output_field=DecimalField())
-                     + 20 * Count('history', filter=Q(
-                        history__car_type=CarType.objects.get(type='Truck')),
-                                  output_field=DecimalField())) * F(
-                'salary') / 100
-        )
-        return earning
-
+    time_interval = time_dict.get(filter_arg)
     history_date = f"history__date__{filter_arg}"
 
+    earning_by_sedan = 10 * Count('history', filter=gen_query(
+                                  'Sedan', history_date, time_interval),
+                                  output_field=DecimalField())
+
+    earning_by_minivan = 15 * Count('history', filter=gen_query(
+                                    'Minivan', history_date, time_interval),
+                                    output_field=DecimalField())
+
+    earning_by_truck = 20 * Count('history', filter=gen_query(
+                                  'Truck', history_date, time_interval),
+                                  output_field=DecimalField())
+
     earning = Washer.objects.annotate(
-        **{filter_arg: (10 * Count('history', filter=
-        Q(**{'history__car_type': CarType.objects.get(type='Sedan'),
-             history_date: current}), output_field=DecimalField())
-                        + 15 * Count('history', filter=Q(
-                    **{'history__car_type': CarType.objects.get(type='Minivan'),
-                       history_date: current}), output_field=DecimalField())
-                        + 20 * Count('history', filter=Q(
-                    **{'history__car_type': CarType.objects.get(type='Truck'),
-                       history_date: current}),
-                                     output_field=DecimalField())) * F(
-            'salary') / 100}
-    )
+        **{filter_arg: (earning_by_sedan + earning_by_minivan +
+                        earning_by_truck) * F('salary') / 100})
 
     return earning
