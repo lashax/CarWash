f4b97507be02de3f3f928f40572b742a7570145b
Lasha Sherozia <repeop0@gmail.com>
2021-02-07 13:00:00 +0400

    Added more encapsulation by defining another function. Also, used typing hints. Defined constants for sedan/minivan/truck washing price.

--- washing_service/view_helpers/washers.py
+++ washing_service/view_helpers/washers.py
@@ -1,11 +1,17 @@
 from datetime import datetime
+from typing import Union
 
-from django.db.models import Count, Q, F, DecimalField
+from django.db.models import Count, Q, F, DecimalField, QuerySet
 
-from washing_service.models import CarType, Washer
+from washing_service.models import Washer
+
+SEDAN_EARNING = 10
+MINIVAN_EARNING = 15
+TRUCK_EARNING = 20
 
 
-def gen_query(car_type, history_date, time_interval):
+def gen_query(car_type: str, history_date: str,
+              time_interval: Union[int, None]) -> Q:
     """
     Generate query, Q object which will be passed to Count class as
     filter argument.
@@ -21,14 +27,40 @@ def gen_query(car_type, history_date, time_interval):
     """
     if time_interval:
         return Q(**{
-            'history__car_type': CarType.objects.get(type=car_type),
+            'history__car_type__type': car_type,
             history_date: time_interval
         })
     else:
-        return Q(**{'history__car_type': CarType.objects.get(type=car_type)})
+        return Q(**{'history__car_type__type': car_type})
 
 
-def earning_by_time(filter_arg):
+def earning_by_car(car_type: str, history_date: str,
+                   time_interval: Union[int, None]) -> Count:
+    """
+    Calculate money earned for given car type.
+
+    car_type is type of a car with which to filter history. This should be
+    one of: 'Sedan', 'Minivan, 'Truck'.
+
+    For sedan, per wash costs 10₾, for minivan 15₾ and for truck 20₾.
+
+    Return CombinedExpression, which is Count object multiplied
+    by value (10, 15 or 20).
+    """
+    car_earning = Count('history', filter=
+                        gen_query(car_type, history_date, time_interval),
+                        output_field=DecimalField())
+    price_per_wash = {
+        'Sedan': SEDAN_EARNING,
+        'Minivan': MINIVAN_EARNING,
+        'Truck': TRUCK_EARNING
+    }
+    car_earning *= price_per_wash.get(car_type)
+
+    return car_earning
+
+
+def earning_by_time(filter_arg: str) -> QuerySet:
     """
     Given 'filter_arg' variable, which is either
     year, month, week or overall, calculate each washers' earning by
@@ -39,6 +71,10 @@ def earning_by_time(filter_arg):
     with month, attribute name will be month). This data stores value in decimal
     as to how much has the washer earned in the given interval, also considering
     salary %.
+
+    Note, that even though type of new attribute is decimal, it will never
+    have fractional part because we are diving the last result by 100,
+    which is an integer.
     """
     time_dict = {
         'overall': None,
@@ -50,17 +86,9 @@ def earning_by_time(filter_arg):
     time_interval = time_dict.get(filter_arg)
     history_date = f"history__date__{filter_arg}"
 
-    earning_by_sedan = 10 * Count('history', filter=gen_query(
-                                  'Sedan', history_date, time_interval),
-                                  output_field=DecimalField())
-
-    earning_by_minivan = 15 * Count('history', filter=gen_query(
-                                    'Minivan', history_date, time_interval),
-                                    output_field=DecimalField())
-
-    earning_by_truck = 20 * Count('history', filter=gen_query(
-                                  'Truck', history_date, time_interval),
-                                  output_field=DecimalField())
+    earning_by_sedan = earning_by_car('Sedan', history_date, time_interval)
+    earning_by_minivan = earning_by_car('Minivan', history_date, time_interval)
+    earning_by_truck = earning_by_car('Truck', history_date, time_interval)
 
     earning = Washer.objects.annotate(
         **{filter_arg: (earning_by_sedan + earning_by_minivan +
